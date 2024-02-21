2fcad7963c21bea1d5bdd22ddf56a370257f3435
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-21 22:20:32 +0700

    анимация при переходе на др. страницу - круг образуется

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,8 +7,8 @@ import { Body } from "../components/Body/Body";
 const Contact = () => {
   return (
     <div className="page contact">
-      <Title>Contact</Title>
-      <Body>
+      <Title delay={0.75}>Contact</Title>
+      <Body delay={0.75}>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam nemo
         facere commodi ipsum labore. Officia, quia ullam exercitationem iure
         sapiente suscipit architecto voluptatum eaque itaque molestiae
@@ -16,14 +16,27 @@ const Contact = () => {
         tempore quibusdam aperiam sed aut minus nulla quisquam ex nihil.
         Molestiae neque reprehenderit cumque laudantium quo!
       </Body>
-      <motion.div className='slide'
-      initial={{y: '-100%'}} animate={{y: '100%'}} transition={{duration: 2, ease: [0.2, 1, 0.2, 1]}}
+      <motion.div
+        initial={{ y: "-100%" }}
+        animate={{
+          y: "100%",
+        }}
+        transition={{ duration: 2, ease: [0.2, 1, 0.2, 1] }} // Cubic bezier curve for easing
+        className="slide"
       />
-      <motion.div 
-      animate={{height: 0, width: 0, borderRadius: "100%"}}
-      exit={{height: '100%', width: '100%', borderRadius: 0}}
-      transition={{duration: 0.5, ease: 'easeInOut'}}
-      className="circle"
+      <motion.div
+        animate={{
+          width: "0",
+          height: "0",
+          borderRadius: "100%",
+        }}
+        exit={{
+          width: "100%",
+          height: "100%",
+          borderRadius: "0",
+        }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className="circle"
       />
     </div>
   );
